83fcfab59da4904eadb38e516acb2b072b1cb457
BradyDouthit <brady.douthit99@gmail.com>
2024-09-28 18:33:18 -0400

    wip termui

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# cli-listener
-CLI tool that simply logs all of your CLI commands to the terminal as you write them.
+# trendify
--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"clilistener/utils/shell"
 	"fmt"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"strconv"
+	"trendify/utils/shell"
 )
 
 func main() {
+
+	if err := ui.Init(); err != nil {
+		fmt.Errorf("failed to initialize termui: %w", err)
+	}
+	defer ui.Close()
+
+	p := widgets.NewParagraph()
+	p.Text = "Hello World!"
+	p.SetRect(0, 0, 25, 5)
+
+	ui.Render(p)
+
+	for e := range ui.PollEvents() {
+		if e.Type == ui.KeyboardEvent {
+			break
+		}
+	}
+
 	shell, path, err := Shell.DetectShell()
 
 	if err != nil {
