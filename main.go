66ef993b079c4731b1838bd774320a4eabf109ed
Brady Douthit <brady.douthit99@gmail.com>
2024-09-30 18:53:33 -0400

    style text

--- main.go
+++ main.go
@@ -5,9 +5,26 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 func main() {
+	// Primary Text Style
+	primaryStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#F0F0F0")).
+		Background(lipgloss.Color("#1E1E1E"))
+
+	// Command Style
+	commandStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#61AFEF")).
+		Bold(true)
+
+	// Count Style
+	countStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#98C379")).
+		Bold(true)
+
 	includeArgsShort := flag.Bool("A", false, "Include arguments in the output")
 	includeArgsLong := flag.Bool("args", false, "Include arguments in the output")
 	topN := flag.Int("top", 5, "Number of top commands to display")
@@ -27,7 +44,13 @@ func main() {
 	topCommands := Shell.GetTopCommands(history, *topN, *includeArgsShort || *includeArgsLong)
 
 	for _, command := range topCommands {
-		fmt.Printf("You have used %s %d times\n", command.Command, command.Count)
+		output := primaryStyle.Render("You have used ") +
+			commandStyle.Render(command.Command) +
+			primaryStyle.Render(" ") +
+			countStyle.Render(fmt.Sprintf("%d", command.Count)) +
+			primaryStyle.Render(" times")
+
+		fmt.Println(output)
 	}
 
 	os.Exit(0)
