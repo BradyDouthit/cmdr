2dea8526b3942e3c6cf3b7e90c3b41afbe529b3f
Brady Douthit <brady.douthit99@gmail.com>
2024-09-30 20:25:54 -0400

    add flag for mistakes

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	includeArgsShort := flag.Bool("A", false, "Include arguments in the output")
 	includeArgsLong := flag.Bool("args", false, "Include arguments in the output")
+	showMistakesLong := flag.Bool("mistakes", false, "Show mistakes (commands that don't exist in the PATH) in the output")
+	showMistakesShort := flag.Bool("M", false, "Show mistakes (commands that don't exist in the PATH) in the output")
 	topN := flag.Int("top", 5, "Number of top commands to display")
 	flag.Parse()
 	shell, path, err := Shell.DetectShell()
@@ -24,8 +26,14 @@ func main() {
 		panic(err)
 	}
 
-	topCommands := Shell.GetTopCommands(history, *topN, *includeArgsShort || *includeArgsLong)
+	if *showMistakesLong || *showMistakesShort {
+		uniqueCommands := Shell.GetUniqueCommandCounts(history, 999, *includeArgsShort || *includeArgsLong)
+		failedCommands := Shell.GetFailedCommands(uniqueCommands, *topN)
+		UI.RenderMistakes(failedCommands)
+		os.Exit(0)
+	}
 
+	topCommands := Shell.GetUniqueCommandCounts(history, *topN, *includeArgsShort || *includeArgsLong)
 	UI.RenderTopCommands(topCommands)
 
 	os.Exit(0)
